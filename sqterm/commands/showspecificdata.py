13e3a719098e49dd40d810c419a72dbab973f040
Gamerbot1000 <gamerbot202@gmail.com>
2026-01-07 00:49:48 +0100

    Added column filtering
    An early version of column filtering has been added to the 'showspecificdata' command (should be available via pipx in next release)

--- sqterm/commands/showspecificdata.py
+++ sqterm/commands/showspecificdata.py
@@ -29,8 +29,19 @@ def main(state, i):
             q = input("Name of the table you wish to fetch data from: ")
 
         try:
-            command = f"SELECT COUNT(*) FROM {q};"
-            commandex = c.execute(command)
+            columnnames = c.execute(f"SELECT name FROM pragma_table_info('{q}');")
+            columnnames1 = c.fetchall()
+            print('')
+            print("=== Columns in table", q + ": ===")
+            print('')
+            for col in columnnames1:
+                print("     ", col[0])
+            print("")
+            q_col = input("Names of columns you wish to fetch data from (or press ENTER to select all): ")
+            if q_col == "":
+                q_col = "*"
+
+            rowcount_ex = c.execute(f"SELECT COUNT(*) FROM {q};")
             rowcount1 = c.fetchall()
             rowcount = rowcount1[0]
             print(f"Rows available: {rowcount[0]}")
@@ -38,13 +49,16 @@ def main(state, i):
             q1f = str(int(q1)-1)
             q2 = input("Row limit: ")
             q2f = str(int(q2)+1)
-            command1 = f"SELECT * FROM {q} LIMIT {q2f} OFFSET {q1f}"
+            command1 = f"SELECT {q_col} FROM {q} LIMIT {q2f} OFFSET {q1f}"
             row1 = c.execute(command1)
             row2 = c.fetchall()
-                            
-            command2 = "PRAGMA table_info(" + q + ")"
-            column1 = c.execute(command2)
-            columns = [col[1] for col in c.fetchall()]
+
+            if q_col == "*":                
+                command2 = "PRAGMA table_info(" + q + ")"
+                column1 = c.execute(command2)
+                columns = [col[1] for col in c.fetchall()]
+            else:
+                columns = [col.strip() for col in q_col.split(",")]
                             
             print('')
             print(tabulate(row2, headers=columns, tablefmt="grid"))
